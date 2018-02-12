aa9e1bfe5bb07ccaff7fb932497ae83e3fabc246
clarkcuadro <clarkant@hawaii.edu>
2018-02-12 10:21:13 -1000

    int
    made i like to eat plus name
    printed fruit basket

--- SwiftPlayground3.playground/Contents.swift
+++ SwiftPlayground3.playground/Contents.swift
@@ -3,3 +3,35 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+
+var fruitBasket: [String]
+
+
+//simple string variable
+
+var simpleString: String
+
+//Inig
+fruitBasket = ["watermelon", "mango", "applebanana"]
+
+print(fruitBasket)
+
+//for-in loop
+
+for name in fruitBasket{
+//print "I like to eat" before every name in fruitBasket
+print("I like to eat " + name)
+}
+//let's print out item at index 1
+    
+    
+print(fruitBasket[1])
+    
+//let's print out item at index 0
+    
+print(fruitBasket[0])
+    
+
+
