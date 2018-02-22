d4a579c4ed4e23c5b72e335f4c7f621b4c4b7e7e
CM Student <cmstudent@uhwo-acm-09.local>
2018-02-21 14:24:35 -1000

    assignment #4 arrays part 2

--- SwiftPlayground3.playground/Contents.swift
+++ SwiftPlayground3.playground/Contents.swift
@@ -32,6 +32,59 @@ print(fruitBasket[1])
 //let's print out item at index 0
     
 print(fruitBasket[0])
+
+print("I like to eat \(fruitBasket)")
+
+
+
+
+
+//Part 8, Start of Assignment #4
+
+var myfavoriteNumber: [String]
+
+myfavoriteNumber = ["one", "two", "three", "four", "five"]
+print(myfavoriteNumber)
+
+for name in myfavoriteNumber
+{ //print "I like number" before every name in myfavoriteNumber
     
+print("I like number" + name)
+
+}
+
+//let's print out item at index 1
+print(myfavoriteNumber[1])
+
+
+//let's print out item at index 0
+print(myfavoriteNumber[0])
+
+print("I like number \(myfavoriteNumber)")
+
+
+//Remove an element at index
+
+var vec = [10,12,86, 98, 99]
+
+vec.remove(at: 2)
+
+print (vec)
+
+
+
+//Append an element
+
+var numbers = [10, 12, 98]
+
+numbers.append(86)
+numbers.append(contentsOf: [99])
+print (numbers)
+
+
+
+
+
+
 
 
